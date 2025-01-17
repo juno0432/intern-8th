f4407f76c361c59e88d47b547611166ebba52fbe
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-18 01:52:41 +0900

    feat: user클래스 생성
    - 회원을 관리하는 클래스인 user를 생성하였습니다
    - 회원에는 id, username, password, nickname, authorityName, isDeleted(삭제여부)가 있습니다.

---
+++ src/main/java/com/intellipick/intern8th/common/config/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.intellipick.intern8th.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditingConfig {
+}
