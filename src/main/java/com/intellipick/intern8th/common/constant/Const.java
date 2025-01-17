34a8013cfab8e5bd24067f664d67f2eb01124a65
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-18 04:07:34 +0900

    feat: security 및 jwt 구현
    - security 및 jwt를 구현 하였습니다.

---
+++ src/main/java/com/intellipick/intern8th/common/constant/Const.java
@@ -0,0 +1,11 @@
+package com.intellipick.intern8th.common.constant;
+
+public class Const {
+
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    public static final long TOKEN_ACCESS_TIME = 60 * 60 * 1000L;
+
+    public static final String USER_USERNAME = "username";
+    public static final String USER_AUTHORITY_NAME = "authorityName";
+}
