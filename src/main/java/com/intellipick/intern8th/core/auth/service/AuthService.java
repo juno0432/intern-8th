2fb4c5e0c5ef7f1bed79553e978555d79ebb0b40
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-18 04:12:44 +0900

    refactor: 로그인 jwt반환으로 변경
    - 로그인시 jwt 토큰을 반환해주는것으로 변경하였습니다.

--- src/main/java/com/intellipick/intern8th/core/auth/service/AuthService.java
+++ src/main/java/com/intellipick/intern8th/core/auth/service/AuthService.java
@@ -3,14 +3,16 @@ package com.intellipick.intern8th.core.auth.service;
 import static com.intellipick.intern8th.common.exception.ErrorCode.DUPLICATE_USER;
 import static com.intellipick.intern8th.common.exception.ErrorCode.PASSWORD_MISMATCH;
 
-import com.intellipick.intern8th.common.config.PasswordEncoder;
+import com.intellipick.intern8th.common.config.JwtUtil;
 import com.intellipick.intern8th.common.exception.ApplicationException;
 import com.intellipick.intern8th.core.auth.dto.request.SignInUserRequestDto;
 import com.intellipick.intern8th.core.auth.dto.request.SignUpUserRequestDto;
+import com.intellipick.intern8th.core.auth.dto.response.GetTokenResponseDto;
 import com.intellipick.intern8th.core.auth.dto.response.GetUserResponseDto;
 import com.intellipick.intern8th.core.user.domain.User;
 import com.intellipick.intern8th.core.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,7 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public GetUserResponseDto signUp(final SignUpUserRequestDto signUpUserRequestDto) {
         boolean doesUserExist = userRepository.existsByUsername(signUpUserRequestDto.getUsername());
@@ -36,11 +39,16 @@ public class AuthService {
         return GetUserResponseDto.from(user);
     }
 
-    public void signIn(final SignInUserRequestDto signInUserRequestDto) {
+    @Transactional(readOnly = true)
+    public GetTokenResponseDto signIn(final SignInUserRequestDto signInUserRequestDto) {
         User user = userRepository.findByUsernameOrThrow(signInUserRequestDto.getUsername());
 
         if (!passwordEncoder.matches(signInUserRequestDto.getPassword(), user.getPassword())) {
             throw new ApplicationException(PASSWORD_MISMATCH);
         }
+
+        String token = jwtUtil.createToken(user.getId(), user.getUsername(), user.getAuthorityName());
+
+        return GetTokenResponseDto.from(jwtUtil.substringToken(token));
     }
 }
