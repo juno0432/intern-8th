ae8a0ed89ad1759a88e60736e88df2a740139a33
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-18 03:15:37 +0900

    feat: 로그인 구현
    - 로그인 로직을 구현하였습니다
    - 아직 jwt와 security는 설정되어 있지않습니다.

--- src/main/java/com/intellipick/intern8th/core/auth/service/AuthService.java
+++ src/main/java/com/intellipick/intern8th/core/auth/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.intellipick.intern8th.core.auth.service;
 
 import static com.intellipick.intern8th.common.exception.ErrorCode.DUPLICATE_USER;
+import static com.intellipick.intern8th.common.exception.ErrorCode.PASSWORD_MISMATCH;
 
 import com.intellipick.intern8th.common.config.PasswordEncoder;
 import com.intellipick.intern8th.common.exception.ApplicationException;
+import com.intellipick.intern8th.core.auth.dto.request.SignInUserRequestDto;
 import com.intellipick.intern8th.core.auth.dto.request.SignUpUserRequestDto;
 import com.intellipick.intern8th.core.auth.dto.response.GetUserResponseDto;
 import com.intellipick.intern8th.core.user.domain.User;
@@ -20,10 +22,10 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public GetUserResponseDto signUp(SignUpUserRequestDto signUpUserRequestDto) {
-        boolean isExistUser = userRepository.existsByUsername(signUpUserRequestDto.getUsername());
+    public GetUserResponseDto signUp(final SignUpUserRequestDto signUpUserRequestDto) {
+        boolean doesUserExist = userRepository.existsByUsername(signUpUserRequestDto.getUsername());
 
-        if (isExistUser) {
+        if (doesUserExist) {
             throw new ApplicationException(DUPLICATE_USER);
         }
 
@@ -32,5 +34,13 @@ public class AuthService {
         userRepository.save(user);
 
         return GetUserResponseDto.from(user);
+    }
+
+    public void signIn(final SignInUserRequestDto signInUserRequestDto) {
+        User user = userRepository.findByUsernameOrThrow(signInUserRequestDto.getUsername());
+
+        if (!passwordEncoder.matches(signInUserRequestDto.getPassword(), user.getPassword())) {
+            throw new ApplicationException(PASSWORD_MISMATCH);
+        }
     }
 }
