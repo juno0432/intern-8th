af2fda3c800d253ae0033d16282d8a39667c2749
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-17 12:55:06 +0900

    feat: 초기 커밋

---
+++ src/main/java/com/intellipick/intern8th/Intern8thApplication.java
@@ -0,0 +1,13 @@
+package com.intellipick.intern8th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intern8thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Intern8thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intellipick/intern8th/Intern8thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellipick.intern8th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Intern8thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
