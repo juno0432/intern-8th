216279ed5b0f4200e64101d98054e41a283e76bf
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-17 18:45:20 +0900

    feat: 예외의 전역 처리를 하기 위한 클래스 생성
    -예외의 전역 처리를위해 GlobalExceptionHandler 클래스를 생성하였습니다.

---
+++ src/main/java/com/intellipick/intern8th/common/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.intellipick.intern8th.common.exception;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    DUPLICATE_USER(BAD_REQUEST, "이미 존재하는 사용자 입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    ErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
