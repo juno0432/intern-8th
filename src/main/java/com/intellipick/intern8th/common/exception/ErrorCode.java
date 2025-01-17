d8c606c53c5eabffd23ce2e36205e0dd8a00a244
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-18 01:52:41 +0900

    feat: user클래스 생성
    - 회원을 관리하는 클래스인 user를 생성하였습니다
    - 회원에는 id, username, password, nickname, authorityName, isDeleted(삭제여부)가 있습니다.

--- src/main/java/com/intellipick/intern8th/common/exception/ErrorCode.java
+++ src/main/java/com/intellipick/intern8th/common/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    DUPLICATE_USER(BAD_REQUEST, "이미 존재하는 사용자 입니다.");
+    DUPLICATE_USER(BAD_REQUEST, "이미 존재하는 사용자 입니다."),
+    NOT_FOUND_TOKEN(BAD_REQUEST, "토큰을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
---
+++ src/main/java/com/intellipick/intern8th/core/user/domain/UserRole.java
@@ -0,0 +1,29 @@
+package com.intellipick.intern8th.core.user.domain;
+
+import static com.intellipick.intern8th.common.exception.ErrorCode.NOT_FOUND_TOKEN;
+
+import com.intellipick.intern8th.common.exception.ApplicationException;
+import java.util.Arrays;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+
+    ROLE_USER(Authority.USER);
+
+    private final String authorityName;
+
+    public static UserRole from(String role) {
+        return Arrays.stream(UserRole.values())
+                .filter(r -> r.name().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow(() -> new ApplicationException(NOT_FOUND_TOKEN));
+    }
+
+    public static class Authority {
+
+        public static final String USER = "ROLE_USER";
+    }
+}
