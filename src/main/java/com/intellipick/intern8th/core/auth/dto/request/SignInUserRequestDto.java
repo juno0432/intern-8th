a704f4b79096006c022557de6c4c6fa7217c70a5
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-19 00:15:25 +0900

    feat: controller dto swagger 문서 추가
    - swagger에서 확인할 수 있게 내용을 추가했습니다.

--- src/main/java/com/intellipick/intern8th/core/auth/dto/request/SignInUserRequestDto.java
+++ src/main/java/com/intellipick/intern8th/core/auth/dto/request/SignInUserRequestDto.java
@@ -1,5 +1,6 @@
 package com.intellipick.intern8th.core.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -7,11 +8,15 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(name = "SignInUserRequestDto - 사용자 로그인 요청 DTO",
+        description = "사용자 로그인 요청 DTO")
 public class SignInUserRequestDto {
 
+    @Schema(title = "이름", description = "이름", example = "james")
     @NotBlank(message = "이름은 필수입니다.")
     private String username;
 
+    @Schema(title = "비밀번호", description = "비밀번호", example = "12345")
     @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
 }
--- src/main/java/com/intellipick/intern8th/core/auth/dto/request/SignUpUserRequestDto.java
+++ src/main/java/com/intellipick/intern8th/core/auth/dto/request/SignUpUserRequestDto.java
@@ -1,5 +1,6 @@
 package com.intellipick.intern8th.core.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -7,14 +8,19 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(name = "SignUpUserRequestDto - 사용자 회원가입 요청 DTO",
+        description = "사용자 회원가입 요청 DTO")
 public class SignUpUserRequestDto {
 
+    @Schema(title = "이름", description = "이름", example = "james")
     @NotBlank(message = "이름은 필수입니다.")
     private String username;
 
+    @Schema(title = "비밀번호", description = "비밀번호", example = "12345")
     @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
 
+    @Schema(title = "닉네임", description = "닉네임", example = "jame")
     @NotBlank(message = "닉네임은 필수입니다.")
     private String nickname;
 }
