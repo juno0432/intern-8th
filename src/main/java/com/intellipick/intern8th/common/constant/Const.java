c0d1c68707c4853d43a235ce3929ca2f33fef8d9
juno0432 <129417004+juno0432@users.noreply.github.com>
2025-01-18 11:05:48 +0900

    feat: refresh token 생성 로직 추가.
    - jwtUtil에 refresh token 생성 로직을 추가하였습니다

--- src/main/java/com/intellipick/intern8th/common/constant/Const.java
+++ src/main/java/com/intellipick/intern8th/common/constant/Const.java
@@ -1,10 +1,15 @@
 package com.intellipick.intern8th.common.constant;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Const {
 
     public static final String BEARER_PREFIX = "Bearer ";
 
     public static final long TOKEN_ACCESS_TIME = 60 * 60 * 1000L;
+    public static final long TOKEN_REFERSH_TIME = 7 * 24 * 60 * 60 * 1000L;
 
     public static final String USER_USERNAME = "username";
     public static final String USER_AUTHORITY_NAME = "authorityName";
